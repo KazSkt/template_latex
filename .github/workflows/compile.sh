64de1f46eb5ccba2183359661e98d7a5baba27fe
KazSkt <sakata@iplab.cs.tsukuba.ac.jp>
2022-04-09 21:41:09 +0900

    リリースできるように変更

--- .github/workflows/compile.sh
+++ .github/workflows/compile.sh
@@ -1,2 +1,7 @@
 cd src
-docker run -v $PWD:/working -w /working texlive/texlive latexmk paper.tex
+texfilename=$(echo "${FILE_NAME}" | cut -f 1 -d '.')
+docker run -v $PWD:/working -w /working texlive/texlive latexmk ${texfilename}
+export PDF_NAME="${texfilename}-$(echo ${GITHUB_REF:10}).pdf"
+mv "${texfilename}.pdf" ${PDF_NAME}
+cd ..
+echo "::set-env name=PDF_NAME::$PDF_NAME"
